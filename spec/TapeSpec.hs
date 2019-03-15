ed9de9e3c575c98c69b75f63a24a04de4e933554
Tejas Bubane <tejasbubane@gmail.com>
2019-03-15 23:44:21 +0530

    Implement Functor instance for Tape
    Just an exercise

--- spec/TapeSpec.hs
+++ spec/TapeSpec.hs
@@ -1,7 +1,20 @@
 module TapeSpec where
 
 import Test.Hspec
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 import Tape
+
+instance Arbitrary a => Arbitrary (Tape a) where
+  arbitrary = do
+    ls <- arbitrary
+    pivot <- arbitrary
+    rs <- arbitrary
+    return $ Tape ls pivot rs
+
+instance Eq a => EqProp (Tape a) where
+  (=-=) = eq
 
 specs :: SpecWith ()
 specs = describe "Tape" $ do
@@ -13,3 +26,7 @@ specs = describe "Tape" $ do
     it "moves pivot to left" $ do
       moveLeft (Tape [0] 1 [0]) `shouldBe` Tape [] 0 [1,0]
       moveLeft (Tape [0,1,2] 3 [4,5,6]) `shouldBe` Tape [1,2] 0 [3,4,5,6]
+
+  let tapeTrigger = undefined :: Tape (Int, Double, Char)
+  it "has well-defined functor instance" $ property $ do
+    quickBatch $ functor tapeTrigger
--- src/Tape.hs
+++ src/Tape.hs
@@ -14,3 +14,6 @@ moveRight (Tape ls x (r:rs)) = Tape (x:ls) r rs
 
 moveLeft :: Tape a -> Tape a
 moveLeft (Tape (l:ls) x rs) = Tape ls l (x:rs)
+
+instance Functor Tape where
+  fmap f (Tape ls x rs) = Tape (fmap f ls) (f x) (fmap f rs)
